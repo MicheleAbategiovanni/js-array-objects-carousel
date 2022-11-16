945ba0d0d670279744e7b76913df8a0815a37c0f
Michele Abategiovanni <michele.abategiovanni@gmail.com>
2022-11-16 22:14:38 +0100

    complete code

--- js/main.js
+++ js/main.js
@@ -62,3 +62,35 @@ scrollNextEl.addEventListener("click", function () {
     newTitleElements.classList.add("opacity-title");
     newDescriptionElements.classList.add("opacity-description"); 
 });
+
+scrollPrevEl.addEventListener("click", function (){
+    const imgElements = document.querySelectorAll("img");
+    const titleElements = document.querySelectorAll("h4");
+    const descriptionElements = document.querySelectorAll("p");
+
+    const oldImg = document.querySelector(".opacity");
+    const oldTitle = document.querySelector(".opacity-title");
+    const oldDescription = document.querySelector(".opacity-description");
+
+    m--;
+
+    if (m < 0) {
+        m = imgElements.length - 1;
+    }
+
+    console.log(oldImg)
+
+    oldImg.classList.remove("opacity");
+    oldTitle.classList.remove("opacity-title");
+    oldDescription.classList.remove("opacity-description"); 
+
+
+    const newActiveEl = imgElements[m];
+    const newTitleElements = titleElements[m];
+    const newDescriptionElements = descriptionElements[m];
+
+
+    newActiveEl.classList.add("opacity");
+    newTitleElements.classList.add("opacity-title");
+    newDescriptionElements.classList.add("opacity-description"); 
+});
